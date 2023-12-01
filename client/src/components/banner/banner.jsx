a74a4430f30791f3bfa565fe41ade49b39717f5b
manuel garcia sarza <manuelgarciasarza@gmail.com>
2023-12-01 16:45:46 -0300

    banner prueba

--- client/src/components/banner/banner.jsx
+++ client/src/components/banner/banner.jsx
@@ -2,6 +2,8 @@ import bannerCuatro from "../../img/bannerCuatro.jpg";
 import bannerCinco from "../../img/bannerCinco.jpg";
 import bannerSeis from "../../img/bannerSeis.jpg";
 import bannerTres from "../../img/bannerTres.jpg";
+import bannerPrueba from "../../img/bannerPrueba.jpg";
+import bannerPrueba2 from "../../img/bannerPrueba2.jpg";
 
 import style from "./banner.module.css";
 
@@ -24,6 +26,8 @@ function Banner() {
       <img className={style.img} src={bannerCuatro} alt="" />
       <img className={style.img} src={bannerCinco} alt="" />
       <img className={style.img} src={bannerSeis} alt="" />
+      <img className={style.img} src={bannerPrueba} alt="" />
+      <img className={style.img} src={bannerPrueba2} alt="" />
     </Carousel>
   );
 }
